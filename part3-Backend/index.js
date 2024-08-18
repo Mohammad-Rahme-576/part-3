09f0ea6895ff009d4df316b3e434a521f2b9948e
Mohammad-Rahme-576 <mohammadrahme576@gmail.com>
2024-08-18 08:46:54 +0300

    part d exercises 3.19 -> 3.21

--- part3-Backend/index.js
+++ part3-Backend/index.js
@@ -191,26 +191,26 @@ app.post('/api/persons', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number }, 
+    { new: true, runValidators: true, context: 'query' }
+  )
+  .then(updatedPerson => {
+    if (updatedPerson) {
+      response.json(updatedPerson);
+    } else {
+      response.status(404).end();
+    }
+  })
+  .catch(error => {
+    console.error('Error in PUT /api/persons/:id:', error.message);
+    next(error);
+  });
+});
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
-    .then(updatedPerson => {
-      if (updatedPerson) {
-        response.json(updatedPerson)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => {
-      console.error('Error in PUT /api/persons/:id:', error.message)
-      next(error)
-    })
-})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -225,8 +225,8 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
-
-  response.status(500).json({ error: 'An unexpected error occurred' })
+  next(error)
+  // response.status(500).json({ error: 'An unexpected error occurred' })
 }
 app.use(errorHandler)
 
@@ -234,3 +234,4 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
--- part3-Backend/models/person.js
+++ part3-Backend/models/person.js
@@ -12,10 +12,25 @@ mongoose.connect(url)
   .catch(error => {
     console.log('error connecting to MongoDB:', error.message);
   });
+//exercise 3.20 part d 
+  const phoneNumberValidator = {
+    validator: function (v) {
+      return /^\d{2,3}-\d+$/.test(v) && v.length >= 8;
+    },
+    message: props => `${props.value} is not a valid phone number! It should have two parts separated by a hyphen, with the first part having 2-3 digits and the total length being at least 8.`
+  };
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name:{
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: phoneNumberValidator,  
+    required: true
+  },
 });
 
 personSchema.set('toJSON', {
